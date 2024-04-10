bf31da5a295e88d47b10db06d1438a602d7eaebb
Guilherme Kersten da Silva <kersten.kdasilva@gmail.com>
2024-04-10 09:21:49 -0300

    ADD: Check Atlas URI string

--- backend/index.js
+++ backend/index.js
@@ -10,9 +10,13 @@ app.use(routes)
 
 const PORT = process.env.PORT || 5555
 
+if (!process.env.DB_URI) {
+    console.log("Sem URI de conexão ao MongoDB Atlas no .env")
+}
+
 mongoose.connect(process.env.DB_URI)
     .then(() => {
-        console.log("Conectado ao banco de dados com sucesso")
+        console.log("Conectado ao MongoDB Atlas com sucesso")
         app.listen(PORT, () => {
             console.log(`Servidor rodando na porta ${PORT}`)
         })
