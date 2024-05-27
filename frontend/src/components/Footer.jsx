eafd9ce6c9d3cca8fe7963ebff0c38f74a6fc734
Guilherme Kersten da Silva <kersten.kdasilva@gmail.com>
2024-05-27 10:43:49 -0300

    estilar melhor a navbar

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,25 +1,28 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
-        // TODO: Refazer o footer por completo, do jeito que está agora está muito bagunçado
-        <footer className="text-muted py-5">
+        <footer className="text-muted py-5 border-top">
             <div className="container">
-                <p className="float-end mb-1">
-                    <Link to={'https://github.com/kerstenbr/QuatenusMyChoice'} target="_blank" rel="noopener noreferrer">Repo do Projeto</Link>
-                </p>
-                <p className="mb-1">&copy; Quatenus MyChoice</p>
-                <p className="float-end mb-1">
-                    <Link to={'https://qbm01.quatenus-system.com.br/quatenus10/QBM/Login/Login.aspx'} target="_blank" rel="noopener noreferrer">Reporte um bug</Link>
-                </p>
-
-                <p>Criado por <Link to={'https://kerstenbr.github.io/SitePei/'} target="_blank" rel="noopener noreferrer">Projetos e Inovações</Link>
-                    <span><br />Com ajuda de  <Link to={'https://github.com/codebruno'} target="_blank" rel="noopener noreferrer">Bruno Silva</Link>, <Link to={'https://github.com/EricSemE'} target="_blank" rel="noopener noreferrer">Eric Caetano</Link> e <Link to={'https://github.com/lucind0'} target="_blank" rel="noopener noreferrer">Kauã Lucindo</Link></span>
-                </p>
-                <p>v0.0.0 - Em testes</p>
+                <div className="row">
+                    <div className="col-md-6 mb-3">
+                        <p className="mb-1">&copy; 2024 Quatenus MyChoice</p>
+                        <p>v0.0.0 - Em testes</p>
+                        <p className="m-0">Criado por <Link to={'https://kerstenbr.github.io/SitePei/'} target="_blank" rel="noopener noreferrer">Projetos e Inovações</Link></p>
+                        <p>Com ajuda de <Link to={'https://github.com/codebruno'} target="_blank" rel="noopener noreferrer">Bruno Silva</Link>, <Link to={'https://github.com/EricSemE'} target="_blank" rel="noopener noreferrer">Eric Caetano</Link> e <Link to={'https://github.com/lucind0'} target="_blank" rel="noopener noreferrer">Kauã Lucindo</Link></p>
+                    </div>
+                    <div className="col-md-6 mb-3 text-md-end">
+                        <p className="mb-1">
+                            <Link to={'https://github.com/kerstenbr/QuatenusMyChoice'} target="_blank" rel="noopener noreferrer">Repo do Projeto</Link>
+                        </p>
+                        <p className="mb-1">
+                            <Link to={'https://qbm01.quatenus-system.com.br/quatenus10/QBM/Login/Login.aspx'} target="_blank" rel="noopener noreferrer">Reporte um bug</Link>
+                        </p>
+                    </div>
+                </div>
             </div>
-        </footer >
-    )
+        </footer>
+    );
 }
 
-export default Footer
+export default Footer;
