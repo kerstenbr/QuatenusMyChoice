06ce6669b61cbab02c555006a64170e8b2cbd4d6
Guilherme Kersten da Silva <kersten.kdasilva@gmail.com>
2024-05-29 11:13:19 -0300

    arrumar a versão que aparece no footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
                 <div className="row">
                     <div className="col-md-6 mb-3">
                         <p className="mb-1">&copy; 2024 Quatenus MyChoice</p>
-                        <p>v0.0.0 - Em testes</p>
+                        <p>v1.0.0</p>
                         <p className="m-0">Criado por <Link to={'https://kerstenbr.github.io/SitePei/'} target="_blank" rel="noopener noreferrer">Projetos e Inovações</Link></p>
                         <p>Com ajuda de <Link to={'https://github.com/codebruno'} target="_blank" rel="noopener noreferrer">Bruno Silva</Link>, <Link to={'https://github.com/EricSemE'} target="_blank" rel="noopener noreferrer">Eric Caetano</Link> e <Link to={'https://github.com/lucind0'} target="_blank" rel="noopener noreferrer">Kauã Lucindo</Link></p>
                     </div>
