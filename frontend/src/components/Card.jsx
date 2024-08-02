582a81b297b64c09c1939f8325546f2060e83a2f
Guilherme Kersten da Silva <kersten.kdasilva@gmail.com>
2024-08-02 11:43:06 -0300

    adicionar badge de 'novo' caso o produto tenha 2 semanas desde a sua criação

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,19 +1,35 @@
 import { Link } from "react-router-dom";
+import Novo from "../assets//novo.png";
 import LimitedParagraph from "./LimitedParagraph";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../context/userContext";
 
 // TODO: Adicionar páginação
 
 const Card = ({ family }) => {
   const { user } = useContext(UserContext);
+  const [createdWeeks, setCreatedWeeks] = useState(false);
+
+  useEffect(() => {
+    const currentDate = new Date();
+    const creationDate = new Date(family.createdAt);
+    const twoWeeksInMilliseconds = 14 * 24 * 60 * 60 * 1000;
+
+    if (currentDate - creationDate <= twoWeeksInMilliseconds) {
+      console.log(`Família criada há duas semanas: ${family.name}`);
+      setCreatedWeeks(true);
+    } else {
+      console.log(`Família antiga: ${family.name}`);
+      setCreatedWeeks(false);
+    }
+  }, [family.name]);
 
   return (
     // TODO: Melhorar a responsividade do card em si, principalmente em telas menores aonde só aparece dois cards um do lado do outro
     <div className="col" style={{ minWidth: "300px" }}>
       <div className="card shadow-sm" style={{ height: "450px" }}>
         <img className="card-img-top" width="100%" height="225" src={family.bannerLink} />
-
+        {createdWeeks ? <img className="position-absolute top-0 end-0" src={Novo} /> : null}
         <div className="card-body">
           <h3>{family.name}</h3>
           {/* <p className="card-text">{family.desc}</p> */}
