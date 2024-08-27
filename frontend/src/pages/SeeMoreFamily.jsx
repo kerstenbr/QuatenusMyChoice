92b522828589cd7598b97ecd63bfbc1db3bff23e
Guilherme Kersten da Silva <kersten.kdasilva@gmail.com>
2024-08-27 13:35:58 -0300

    adicionar campo de obsvações

--- frontend/src/pages/SeeMoreFamily.jsx
+++ frontend/src/pages/SeeMoreFamily.jsx
@@ -76,6 +76,20 @@ const SeeMoreFamily = () => {
               </div>
             )}
 
+            {family.observations ? (
+              <div className="alert alert-warning p-2" role="alert">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="m-0 p-0 bi bi-exclamation-circle float-end" viewBox="0 0 16 16">
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                  <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
+                </svg>
+                <div>
+                  {family.observations.split('\n').map((text, index) => (
+                    <p className="m-0" key={index}>{text}</p>
+                  ))}
+                </div>
+                
+              </div>) : (null)}
+
             {family.canvaLink ? (
               <>
                 <img style={{ width: "100%" }} src={family.canvaLink} alt="Canva" />
