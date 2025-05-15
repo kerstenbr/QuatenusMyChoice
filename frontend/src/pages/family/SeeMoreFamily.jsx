95727d449fecfd8ce001a378abbb6c2bdc9616ef
Guilherme Kersten da Silva <kersten.kdasilva@gmail.com>
2025-05-15 10:53:32 -0300

    adicionando ícones SVG aos botões de ação e melhorando a estrutura do layout nas páginas de edição e visualização da família

--- frontend/src/pages/family/SeeMoreFamily.jsx
+++ frontend/src/pages/family/SeeMoreFamily.jsx
@@ -80,24 +80,32 @@ const SeeMoreFamily = () => {
   return (
     <div className="py-2 bg-light">
       <div className="container">
-        <button className="btn btn-sm btn-qorange mb-2 float-end" onClick={goBack}>
-          Voltar
-        </button>
-        {user && user.admin === true ? (
-          <>
-            <Link to={`/family/edit/${family._id}`}>
-              <button type="button" className="btn btn-sm btn-warning me-1 text-white float-end">
-                Editar
-              </button>
-            </Link>
-          </>
-        ) : null}
-
+        <div className="sticky-top" style={{ top: "80px", zIndex: "1" }}>
+          <button className="btn btn-sm btn-qorange mb-2 float-end" onClick={goBack}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-return-left mb-1" viewBox="0 0 16 16">
+              <path
+                fill-rule="evenodd"
+                d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5"
+              />
+            </svg>
+          </button>
+          {user && user.admin === true ? (
+            <>
+              <Link to={`/family/edit/${family._id}`}>
+                <button type="button" className="btn btn-sm btn-warning me-1 text-white float-end">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil" viewBox="0 0 16 16">
+                    <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
+                  </svg>
+                </button>
+              </Link>
+            </>
+          ) : null}
+        </div>
         <div>
           <h3 className="mt-1">
             {family.name} - {family.qbmCode}
           </h3>
-          <hr className="mt-0" />
+          <hr className="mt-0 shadow" />
         </div>
 
         <div>
@@ -130,16 +138,16 @@ const SeeMoreFamily = () => {
 
         {family.observations ? (
           <div className="alert alert-warning p-2" role="alert">
-            {/* <svg
+            <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
               height="16"
               fill="currentColor"
-              className="m-0 p-0 bi bi-exclamation-circle float-end"
+              class="bi bi-exclamation-triangle float-start mt-1 me-1"
               viewBox="0 0 16 16">
-              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-              <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z" />
-            </svg> */}
+              <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.15.15 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.2.2 0 0 1-.054.06.1.1 0 0 1-.066.017H1.146a.1.1 0 0 1-.066-.017.2.2 0 0 1-.054-.06.18.18 0 0 1 .002-.183L7.884 2.073a.15.15 0 0 1 .054-.057m1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767z" />
+              <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z" />
+            </svg>
             <p className="p-0 m-0 fw-bold">Atenção:</p>
             <div>
               {family.observations.split("\n").map((text, index) => (
@@ -160,112 +168,110 @@ const SeeMoreFamily = () => {
           </>
         ) : null}
 
-        <div>
-          {hasTelemetry && (
-            <div>
-              <div className="row">
-                <h4>Telemetria</h4>
-                <div className="col-6">
-                  <div className="table-responsive">
-                    <table className="table table-bordered table-hover">
-                      <thead>
-                        <tr>
-                          <th>Acessório Digital</th>
-                          <th>Nº de Entradas Usadas</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <td>SENSOR RPM</td>
-                          <td>1</td>
-                        </tr>
-                        <tr>
-                          <td>SENSOR EMB</td>
-                          <td>1</td>
-                        </tr>
-                        <tr>
-                          <td>SENSOR FREIO</td>
-                          <td>1</td>
-                        </tr>
-                        <tr>
-                          <td>SENSOR PORTA</td>
-                          <td>1</td>
-                        </tr>
-                        <tr>
-                          <td>SENSOR BAÚ</td>
-                          <td>1</td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-
-                <div className="col-6">
-                  <div className="table-responsive">
-                    <table className="table table-bordered table-hover">
-                      <thead>
-                        <tr>
-                          <th>Acessório Analógico</th>
-                          <th>Nº de Entradas Usadas</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <td>SENSOR MOTOR</td>
-                          <td>1</td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-              </div>
-
-              <div>
+        {hasTelemetry && (
+          <div>
+            <div className="row">
+              <h4>Telemetria</h4>
+              <div className="col-6">
                 <div className="table-responsive">
                   <table className="table table-bordered table-hover">
                     <thead>
                       <tr>
-                        <th scope="col">Produto</th>
-                        <th colSpan="2" className="text-center">
-                          Entradas Livres
-                        </th>
-                      </tr>
-                      <tr>
-                        <th scope="col"></th>
-                        <th scope="col" className="text-center">
-                          Digital
-                        </th>
-                        <th scope="col" className="text-center">
-                          Analógica
-                        </th>
+                        <th>Acessório Digital</th>
+                        <th>Nº de Entradas Usadas</th>
                       </tr>
                     </thead>
                     <tbody>
-                      {family.products.map((product) => {
-                        if (product.telemetry) {
-                          return (
-                            <tr key={product.name}>
-                              <td>
-                                <p className="m-0 p-0" style={{ fontSize: "12px" }}>
-                                  {product.qbmCode}
-                                </p>
-                                <p className="m-0 p-0">{product.name}</p>
-                              </td>
-                              <td className="text-center">{product.telemetry.digital}</td>
-                              <td className="text-center">{product.telemetry.analog}</td>
-                            </tr>
-                          );
-                        }
-                        return null;
-                      })}
+                      <tr>
+                        <td>SENSOR RPM</td>
+                        <td>1</td>
+                      </tr>
+                      <tr>
+                        <td>SENSOR EMB</td>
+                        <td>1</td>
+                      </tr>
+                      <tr>
+                        <td>SENSOR FREIO</td>
+                        <td>1</td>
+                      </tr>
+                      <tr>
+                        <td>SENSOR PORTA</td>
+                        <td>1</td>
+                      </tr>
+                      <tr>
+                        <td>SENSOR BAÚ</td>
+                        <td>1</td>
+                      </tr>
                     </tbody>
                   </table>
                 </div>
               </div>
-              <hr />
+
+              <div className="col-6">
+                <div className="table-responsive">
+                  <table className="table table-bordered table-hover">
+                    <thead>
+                      <tr>
+                        <th>Acessório Analógico</th>
+                        <th>Nº de Entradas Usadas</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>SENSOR MOTOR</td>
+                        <td>1</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
             </div>
-          )}
-        </div>
+
+            <div>
+              <div className="table-responsive">
+                <table className="table table-bordered table-hover">
+                  <thead>
+                    <tr>
+                      <th scope="col">Produto</th>
+                      <th colSpan="2" className="text-center">
+                        Entradas Livres
+                      </th>
+                    </tr>
+                    <tr>
+                      <th scope="col"></th>
+                      <th scope="col" className="text-center">
+                        Digital
+                      </th>
+                      <th scope="col" className="text-center">
+                        Analógica
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {family.products.map((product) => {
+                      if (product.telemetry) {
+                        return (
+                          <tr key={product.name}>
+                            <td>
+                              <p className="m-0 p-0" style={{ fontSize: "12px" }}>
+                                {product.qbmCode}
+                              </p>
+                              <p className="m-0 p-0">{product.name}</p>
+                            </td>
+                            <td className="text-center">{product.telemetry.digital}</td>
+                            <td className="text-center">{product.telemetry.analog}</td>
+                          </tr>
+                        );
+                      }
+                      return null;
+                    })}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <hr />
+          </div>
+        )}
 
         <div>
           <h4 className="d-flex align-items-center">
@@ -286,10 +292,10 @@ const SeeMoreFamily = () => {
                   </li>
                   {user && (user.admin === true || user.role === "cs") && (
                     <li>
-                    <button className="dropdown-item" onClick={togleRenovationTable}>
-                      Tabela de Renovação
-                    </button>
-                  </li>
+                      <button className="dropdown-item" onClick={togleRenovationTable}>
+                        Tabela de Renovação
+                      </button>
+                    </li>
                   )}
                 </ul>
               )}
@@ -472,61 +478,79 @@ const SeeMoreFamily = () => {
               </table>
             )}
             {showRenovationTable && (
-              <table className="table table-bordered table-hover table-sm">
-                <thead>
-                  <tr>
-                    <th scope="col">Produto</th>
-                    <th className="text-center">12 meses</th>
-                    <th className="text-center">24 meses</th>
-                    <th className="text-center">36 meses</th>
-                    <th scope="col" className="text-center">
-                      Fecho
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {family.products ? (
-                    family.products.map((product) => {
-                      return (
-                        <tr key={product.name} title={product.desc}>
-                          <td>
-                            <p className="m-0 p-0" style={{ fontSize: "12px" }}>
-                              {product.qbmCode}
-                            </p>
-                            <p className="m-0 p-0">{product.name}</p>
-                          </td>
-                          {product.price.renovation[0] ? (
-                            <td className="text-center text-truncate align-middle">{product.price.renovation[0]}</td>
-                          ) : (
-                            <td className="text-center align-middle">N/A</td>
-                          )}
-                          {product.price.renovation[1] ? (
-                            <td className="text-center text-truncate align-middle">{product.price.renovation[1]}</td>
-                          ) : (
-                            <td className="text-center align-middle">N/A</td>
-                          )}
-                          {product.price.renovation[2] ? (
-                            <td className="text-center text-truncate align-middle">{product.price.renovation[2]}</td>
-                          ) : (
-                            <td className="text-center align-middle">N/A</td>
-                          )}
-                          {product.price.closure ? (
-                            <td className="text-center text-truncate align-middle">{product.price.closure}</td>
-                          ) : (
-                            <td className="text-center align-middle">N/A</td>
-                          )}
-                        </tr>
-                      );
-                    })
-                  ) : (
+              <div>
+                <div className="alert alert-warning p-2" role="alert">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-exclamation-triangle float-start mt-1 me-1"
+                    viewBox="0 0 16 16">
+                    <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.15.15 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.2.2 0 0 1-.054.06.1.1 0 0 1-.066.017H1.146a.1.1 0 0 1-.066-.017.2.2 0 0 1-.054-.06.18.18 0 0 1 .002-.183L7.884 2.073a.15.15 0 0 1 .054-.057m1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767z" />
+                    <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z" />
+                  </svg>
+                  <p className="p-0 m-0 fw-bold">Atenção:</p>
+                  <div>
+                    <p className="m-0">Não é permitido conceder valores abaixo dos apresentados nesta tabela sem autorização.</p>
+                  </div>
+                </div>
+                <table className="table table-bordered table-hover table-sm">
+                  <thead>
                     <tr>
-                      <td colSpan="11" className="text-center bg-danger text-white">
-                        Erro
-                      </td>
+                      <th scope="col">Produto</th>
+                      <th className="text-center">12 meses</th>
+                      <th className="text-center">24 meses</th>
+                      <th className="text-center">36 meses</th>
+                      <th scope="col" className="text-center">
+                        Fecho
+                      </th>
                     </tr>
-                  )}
-                </tbody>
-              </table>
+                  </thead>
+                  <tbody>
+                    {family.products ? (
+                      family.products.map((product) => {
+                        return (
+                          <tr key={product.name} title={product.desc}>
+                            <td>
+                              <p className="m-0 p-0" style={{ fontSize: "12px" }}>
+                                {product.qbmCode}
+                              </p>
+                              <p className="m-0 p-0">{product.name}</p>
+                            </td>
+                            {product.price.renovation[0] ? (
+                              <td className="text-center text-truncate align-middle">{product.price.renovation[0]}</td>
+                            ) : (
+                              <td className="text-center align-middle">N/A</td>
+                            )}
+                            {product.price.renovation[1] ? (
+                              <td className="text-center text-truncate align-middle">{product.price.renovation[1]}</td>
+                            ) : (
+                              <td className="text-center align-middle">N/A</td>
+                            )}
+                            {product.price.renovation[2] ? (
+                              <td className="text-center text-truncate align-middle">{product.price.renovation[2]}</td>
+                            ) : (
+                              <td className="text-center align-middle">N/A</td>
+                            )}
+                            {product.price.closure ? (
+                              <td className="text-center text-truncate align-middle">{product.price.closure}</td>
+                            ) : (
+                              <td className="text-center align-middle">N/A</td>
+                            )}
+                          </tr>
+                        );
+                      })
+                    ) : (
+                      <tr>
+                        <td colSpan="11" className="text-center bg-danger text-white">
+                          Erro
+                        </td>
+                      </tr>
+                    )}
+                  </tbody>
+                </table>
+              </div>
             )}
           </div>
         </div>
@@ -542,13 +566,11 @@ const SeeMoreFamily = () => {
           ) : null}
         </div>
 
-        {user.role === "suporte a operações" || user.admin === true ? (
-          <div className="mt-2">
-            <Link to={`${import.meta.env.VITE_GA_LINK}`} target="_blank" rel="noopener noreferrer">
-              Guias de Instalação
-            </Link>
-          </div>
-        ) : null}
+        <div className="mt-2">
+          <Link to={`${import.meta.env.VITE_GA_LINK}`} target="_blank" rel="noopener noreferrer">
+            Guias de Instalação
+          </Link>
+        </div>
 
         <div>
           <hr />
