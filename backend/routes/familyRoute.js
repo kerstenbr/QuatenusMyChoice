c825b1f218c8f141ff97cf85528f915689bbfe8c
Guilherme Kersten da Silva <kersten.kdasilva@gmail.com>
2025-06-16 12:45:32 -0300

    organizição e remoção de coisas que não estão sendo utilizadas

--- backend/routes/familyRoute.js
+++ backend/routes/familyRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import multer from 'multer';
-import { findAll, findById, findByName, createFamily, editFamily, deleteFamily, downloadFamilies, uploadFamilies } from "../controllers/familyController.js";
+import { findAll, findById, createFamily, editFamily, deleteFamily, downloadFamilies, uploadFamilies } from "../controllers/familyController.js";
 import { authenticateUser, isAdmin } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
@@ -8,7 +8,7 @@ const upload = multer({ dest: 'uploads/families' });
 
 router.get("/", authenticateUser, findAll);
 router.get("/download", authenticateUser, isAdmin, downloadFamilies);
-router.get("/search", authenticateUser, findByName);
+// router.get("/search", authenticateUser, findByName);
 router.get("/:id", authenticateUser, findById);
 router.post("/", authenticateUser, isAdmin, createFamily);
 router.post('/upload', authenticateUser, isAdmin, upload.single('file'), uploadFamilies);
