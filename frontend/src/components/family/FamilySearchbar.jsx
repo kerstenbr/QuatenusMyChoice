cc423d827512f825c2c4b2d147d090a0481812a5
Guilherme Kersten da Silva <kersten.kdasilva@gmail.com>
2025-06-17 14:06:30 -0300

    class -> className fill-rule -> fillRule

--- frontend/src/components/family/FamilySearchbar.jsx
+++ frontend/src/components/family/FamilySearchbar.jsx
@@ -34,7 +34,7 @@ const FamilySearchbar = ({ searchTerm, setSearchTerm, tagOptions, selectedTags, 
         <Select isMulti options={tagOptions} value={selectedTags} onChange={setSelectedTags} placeholder="Filtros" />
       </div>
       <button className="btn btn-sm btn-qorange" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-three-dots-vertical" viewBox="0 0 16 16">
           <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
         </svg>
       </button>
